3bdc4d044e3e4f41e9e0b8d07e8f259cc2651ede
Andre Santos <andresantoss0598@gmail.com>
2024-07-16 13:01:52 -0300

    add: pages

---
+++ src/pages/Car/index.tsx
@@ -0,0 +1,8 @@
+
+export default function CarDetail() {
+  return (
+    <div>
+        Pagina de Detalhes
+    </div>  
+  )
+}
---
+++ src/pages/DashBord/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+  return (
+    <div>
+      Pagina de dashbord
+    </div>
+  )
+}
