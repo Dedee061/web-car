9fb5f2e2175892df133c1d63809b55daa039b8ed
Andre Santos <andresantoss0598@gmail.com>
2024-07-31 17:24:04 -0300

    ...

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,21 @@
-
+import logoImg from "../../assets/logo.svg";
+import Container from "../../components/container";
+import { Link } from "react-router-dom";
+import Input from "../../components/Input";
 
 export function Login() {
   return (
-    <div>
-        Pagina Login
-    </div>
-  )
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to={'/'} className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="logo do site" className="w-full"/>
+        </Link>
+
+        <form className="bg-white max-w-xl w-full rounded-lg">
+        <Input type="email" placeholder= "Digite seu email" name="email"/>
+        <Input type="password" placeholder="Digite sua senha" name="password"/>
+        </form>
+      </div>
+    </Container>
+  );
 }
